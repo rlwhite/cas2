2df28d2afaad6b805f0abde2eece7d2d9ad3e2a4
Randy White <rlwhite@cfl.rr.com>
2016-03-20 20:03:14 -0400

    - Added blog entry - Added Command abstract class and TestCommand class

--- src/casTest/casTest.cpp
+++ src/casTest/casTest.cpp
@@ -1,6 +1,41 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+#define printResult(err) cout <<                                 \
+                                 (0 == err ? "All tests PASSED": \
+                                  "One or more tests FAILED") << endl
+
+struct Command
+{
+    virtual ~Command()
+    {}
+
+    virtual bool exec() = 0;
+};
+
+struct TestCommand : Command
+{
+    bool exec()
+    {
+        return true;
+    }
+};
+
+Command* createCommand(int argc, const char* const argv[])
+{
+    return new TestCommand();
+}
 
 int main(int argc, const char* const argv[])
 {
+    int err(0);
+    std::auto_ptr<Command> cmd(createCommand(argc, argv));
 
-    return 0;
+    if(!cmd->exec())
+        ++err;
+
+    printResult(err);
+
+    return err;
 }
